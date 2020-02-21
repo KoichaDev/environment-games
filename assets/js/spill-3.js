c7cfb87e2d5b63a893e468a2366c1188a42523e6
Khoi Hoang <hello@koicha.dev>
2020-02-21 23:36:53 +0100

    spill #3 - Added new image of seal and code optimization
    - The seal image is individual object
    - The image of the food/trash item is in front instead of behind of the seal. The intention is to use event click listener

--- assets/js/spill-3.js
+++ assets/js/spill-3.js
@@ -1,28 +1,28 @@
-document.addEventListener("DOMContentLoaded", e => {
+document.addEventListener('DOMContentLoaded', e => {
   const sealImage = [
-    "./assets/img/spill-3/seal-open.png",
-    "./assets/img/spill-3/seal-close.png"
+    './assets/img/spill-3/seal-open.png',
+    './assets/img/spill-3/seal-close.png'
   ];
   const foodImg = [
-    "./assets/img/spill-3/blue-fish.png",
-    "./assets/img/spill-3/fat-fish.png"
+    './assets/img/spill-3/blue-fish.png',
+    './assets/img/spill-3/fat-fish.png'
   ];
 
   const trashImg = [
-    "./assets/img/spill-3/bread-box.png",
-    "./assets/img/spill-3/soda-box.png"
+    './assets/img/spill-3/bread-box.png',
+    './assets/img/spill-3/soda-box.png'
   ];
 
   const foodElements = foodImg.map(image => {
-    const element = document.createElement("img");
-    element.className = "food-item";
+    const element = document.createElement('img');
+    element.className = 'food-item';
     element.src = image;
     return element;
   });
 
   const trashElements = trashImg.map(image => {
-    const element = document.createElement("img");
-    element.className = "trash-item";
+    const element = document.createElement('img');
+    element.className = 'trash-item';
     element.src = image;
     return element;
   });
@@ -32,34 +32,11 @@ document.addEventListener("DOMContentLoaded", e => {
 
   // Object Items that is animated moving on the right
   mixObjImg.map(img => {
-    const itemObj = document.querySelector(".item-obj");
+    const itemObj = document.querySelector('.item-obj');
     itemObj.appendChild(img);
   });
 
-  /* // setInterval for the seal animation
-  setInterval(() => {
-    const img = document.querySelector(".images");
-    const divImg = document.createElement("img");
-    divImg.classList.add("img-seal");
-    divImg.setAttribute("src", sealImage[0]);
-    img.appendChild(divImg);
-
-    const replace = setTimeout(() => {
-      document.querySelector(".img-seal").src = sealImage[1];
-    }, 1500);
-
-    if (replace) {
-      document.querySelector(".img-seal").src = sealImage[0];
-    }
-
-    // We want to make sure it will not reproduce many pictures
-    if (img.childElementCount > 1) {
-      document.querySelector(".img-seal").remove();
-    }
-  }, 900);
- */
-  const seal = document.querySelector(".img-seal");
-  const foodItem = document.querySelectorAll(".food-item");
-  const trashItem = document.querySelectorAll(".trash-item");
-  console.log(seal);
+  const seal = document.querySelector('.img-seal');
+  const foodItem = document.querySelectorAll('.food-item');
+  const trashItem = document.querySelectorAll('.trash-item');
 });
