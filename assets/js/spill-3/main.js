9727228cf706ae636b94c389bbdabc2c95fd88ed
Khoi Hoang <hello@koicha.dev>
2020-02-22 21:44:31 +0100

    spill #3 - Added object items reversed order
    - The item will be in reverse order when popping out
    - If rolling on the item should be faster, then change the animation: slide-slow on CSS. 
    - If displaying the item object is slower/faster. Edit it on await sleep() function

--- assets/js/spill-3/main.js
+++ assets/js/spill-3/main.js
@@ -28,98 +28,84 @@ document.addEventListener('DOMContentLoaded', e => {
   const mixObjImg = [...foodElements, ...trashElements];
   mixObjImg.sort(() => Math.random() - 0.5);
 
-  // Object Items that is animated moving on the right
-  /* mixObjImg.forEach(img => {
-    const itemObj = document.querySelector('.item-obj');
-
-    //await sleep(3000);
-    itemObj.appendChild(img);
-  }); */
-
   (async () => {
     const itemObj = document.querySelector('.item-obj');
     for (let i = 0; i < mixObjImg.length; i++) {
-      await sleep(1000);
+      await sleep(2000);
+      console.log(mixObjImg[i]);
 
-      itemObj.appendChild(mixObjImg[i]);
-    }
-  })();
+      // Reversing the order of the item to "popping" on the screen
+      itemObj.prepend(mixObjImg[i]);
 
-  //Tutor();
+      const images = document.querySelector('.images');
+      const hiddenWall = document.querySelector('.hidden-wall');
+      const sealHappySad = document.querySelector('.seal-happy-sad');
 
-  const images = document.querySelector('.images');
-  const hiddenWall = document.querySelector('.hidden-wall');
-  const sealHappySad = document.querySelector('.seal-happy-sad');
+      const sealHappy = document.querySelector('.seal-happy');
+      const sealSad = document.querySelector('.seal-sad');
 
-  const sealHappy = document.querySelector('.seal-happy');
-  const sealSad = document.querySelector('.seal-sad');
+      const itemObjects = document.querySelector('.item-obj');
 
-  const itemObjects = document.querySelector('.item-obj');
+      const foodItem = document.querySelectorAll('.food-item');
+      const trashItem = document.querySelectorAll('.trash-item');
 
-  const foodItem = document.querySelectorAll('.food-item');
-  const trashItem = document.querySelectorAll('.trash-item');
+      for (const trashElements of trashItem) {
+        // Checking if the element is overlapping all the time
+        setInterval(() => {
+          if (isOverlapping(trashElements, hiddenWall)) {
+            // remove the trash image objects
+            const displayNone = (trashElements.style.display = 'none');
+            images.firstElementChild.classList.remove('seal-open-close');
 
-  /* for (let i = 0; i < itemObjects.childNodes.length; i++) {
-    const childElement = itemObjects.childNodes[i];
-    console.log(childElement);
-  } */
-  // itemObjects.childNodes.for);
+            if (displayNone) {
+              sealSad.src = './assets/img/spill-3/seal-trist.png';
 
-  for (const trashElements of trashItem) {
-    // Checking if the element is overlapping all the time
-    setInterval(() => {
-      if (isOverlapping(trashElements, hiddenWall)) {
-        // remove the trash image objects
-        const displayNone = (trashElements.style.display = 'none');
-        images.firstElementChild.classList.remove('seal-open-close');
+              itemObjects.style.animationPlayState = 'paused';
+            }
+            // Will restart the item object animation after 1 second
+            setTimeout(() => {
+              itemObjects.style.animationPlayState = 'running';
+              // Remove the src of the img
+              sealSad.src = '';
+              images.firstElementChild.classList.add('seal-open-close');
+            }, 1000);
+          }
+        }, 100);
 
-        if (displayNone) {
-          sealSad.src = './assets/img/spill-3/seal-trist.png';
+        trashElements.addEventListener('click', e => {
+          trashElements.classList.add('trash-item-flying-up');
+          setTimeout(() => (e.target.style.display = 'none'), 300);
+
+          images.firstElementChild.classList.remove('seal-open-close');
+
+          sealHappy.src = './assets/img/spill-3/seal-glad.png';
 
           itemObjects.style.animationPlayState = 'paused';
-        }
-        // Will restart the item object animation after 1 second
-        setTimeout(() => {
-          itemObjects.style.animationPlayState = 'running';
-          // Remove the src of the img
-          sealSad.src = '';
-          images.firstElementChild.classList.add('seal-open-close');
-        }, 1000);
+
+          setTimeout(() => {
+            sealHappy.src = '';
+            itemObjects.style.animationPlayState = 'running';
+            // Remove the src of the img
+            sealSad.src = '';
+            images.firstElementChild.classList.add('seal-open-close');
+          }, 1000);
+        });
       }
-    }, 100);
 
-    trashElements.addEventListener('click', e => {
-      trashElements.classList.add('trash-item-flying-up');
-      setTimeout(() => (e.target.style.display = 'none'), 300);
+      for (const foodElements of foodItem) {
+        // Checking if the element is overlapping all the time
+        setInterval(() => {
+          if (isOverlapping(foodElements, hiddenWall)) {
+            // remove the trash image objects
+            foodElements.style.display = 'none';
+            images.firstElementChild.classList.add('seal-open-close');
+          }
+        }, 100);
 
-      images.firstElementChild.classList.remove('seal-open-close');
-
-      sealHappy.src = './assets/img/spill-3/seal-glad.png';
-
-      itemObjects.style.animationPlayState = 'paused';
-
-      setTimeout(() => {
-        sealHappy.src = '';
-        itemObjects.style.animationPlayState = 'running';
-        // Remove the src of the img
-        sealSad.src = '';
-        images.firstElementChild.classList.add('seal-open-close');
-      }, 1000);
-    });
-  }
-
-  for (const foodElements of foodItem) {
-    // Checking if the element is overlapping all the time
-    setInterval(() => {
-      if (isOverlapping(foodElements, hiddenWall)) {
-        // remove the trash image objects
-        foodElements.style.display = 'none';
-        images.firstElementChild.classList.add('seal-open-close');
+        foodElements.addEventListener('click', e => {
+          // Add heart that will move animation up
+        });
       }
-    }, 100);
-
-    foodElements.addEventListener('click', e => {
-      // Add heart that will move animation up
-    });
-  }
+    }
+  })();
 });
