fcdbe3f327f585a030e89b1d7a2bdc4955f052d8
Khoi Hoang <hello@koicha.dev>
2020-02-26 15:38:04 +0100

    spill #2 - Fixed correct items filter away the img
    - Finn 5 feil is the trash items, not food items

--- assets/js/spill-2.js
+++ assets/js/spill-2.js
@@ -1,61 +1,62 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-    const mat = document.querySelectorAll('.mat');
-    const ikkeMat = document.querySelectorAll('.ikke-mat');
+  const mat = document.querySelectorAll('.mat');
+  const ikkeMat = document.querySelectorAll('.ikke-mat');
 
-    for(const matAvfall of mat) {
-        // Adding the attribute of the items can't be draggable
-        matAvfall.setAttribute('draggable', 'false');
+  for (const matAvfall of mat) {
+    // Adding the attribute of the items can't be draggable
+    matAvfall.setAttribute('draggable', 'false');
 
-        matAvfall.addEventListener('click', (e) => {
-            e.preventDefault();
+    matAvfall.addEventListener('click', e => {
+      e.preventDefault();
 
-            // Added success sound if they click on the correct item
-            const audio = new Audio('./assets/mp3/success-sound.mp3');
-            audio.play();
-            
-            e.target.style.display = 'none';
+      // Added error sound if they click on a wrong item
+      const audio = new Audio('./assets/mp3/error-sound.mp3');
+      audio.play();
 
-            // This will return true if there is no more display is none
-            if(Array.prototype.every.call(mat, ({ style }) => style.display === "none")) {
-                alert('Woohooo! Du er så flink!')
-            }
-        })
-    }
+      // Creating an empty array
+      const imgArr = [];
 
-    for(const ikkeMatAvfall of ikkeMat) {
-        // Adding the attribute of the items can't be draggable
-        ikkeMatAvfall.setAttribute('draggable', 'false');
+      // Pushing the URL path of the wrong image
+      imgArr.push(e.target.src);
 
-        ikkeMatAvfall.addEventListener('click', (e) => {
-            e.preventDefault();
+      // Overwriting the old image src to the x-mark image
+      e.target.src = './assets/img/spill2/x-mark.png';
 
-            // Added error sound if they click on a wrong item
-            const audio = new Audio('./assets/mp3/error-sound.mp3');
-            audio.play();
+      // We want to loop through the array of the original URL path of the image
+      for (const srcPath of imgArr) {
+        const path = new URL(srcPath);
+        console.log(imgArr);
 
-            // Creating an empty array
-            const imgArr = [];
-            
-            // Pushing the URL path of the wrong image
-            imgArr.push(e.target.src);
-            
-            // Overwriting the old image src to the x-mark image
-            e.target.src = './assets/img/spill2/x-mark.png';
-            
-            // We want to loop through the array of the original URL path of the image
-            for(const srcPath of imgArr) {
-                const path = new URL(srcPath)
-               console.log(imgArr)
-                
-               console.log(e.target)
-                // After the image has displayed x-mark, we want to get the original src Path of the image 
-                // from the array we did push at the beginning
-                setTimeout(() => e.target.src = `${path.pathname}`, 1250);
-            }
-        });
-    }
+        console.log(e.target);
+        // After the image has displayed x-mark, we want to get the original src Path of the image
+        // from the array we did push at the beginning
+        setTimeout(() => (e.target.src = `${path.pathname}`), 1250);
+      }
+    });
+  }
 
+  for (const ikkeMatAvfall of ikkeMat) {
+    // Adding the attribute of the items can't be draggable
+    ikkeMatAvfall.setAttribute('draggable', 'false');
+
+    ikkeMatAvfall.addEventListener('click', e => {
+      e.preventDefault();
+
+      // Added success sound if they click on the correct item
+      const audio = new Audio('./assets/mp3/success-sound.mp3');
+      audio.play();
+
+      e.target.style.display = 'none';
+
+      // This will return true if there is no more display is none
+      if (
+        Array.prototype.every.call(
+          ikkeMat,
+          ({ style }) => style.display === 'none'
+        )
+      ) {
+        alert('Woohooo! Du er så flink!');
+      }
+    });
+  }
 });
-
-
