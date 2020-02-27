dde0048a3c6d20ddf2f394ad00a6cdd3196bc3d1
Khoi Hoang <hello@koicha.dev>
2020-02-27 20:17:05 +0100

    spill #1 - Added A/B Test
    - Test A for main picture
    - Test B for prototype picture
    a

---
+++ assets/js/spill-1/main.js
@@ -0,0 +1,35 @@
+import {
+  dragStart,
+  dragEnter,
+  dragOver,
+  dragLeave,
+  drop,
+  dragEnd
+} from './drag-n-drop-functions.mjs';
+
+const draggable = document.querySelectorAll('.draggable');
+const droppable = document.querySelectorAll('.droppable');
+
+document.addEventListener('DOMContentLoaded', () => {
+  for (const dragItems of draggable) {
+    dragItems.addEventListener('dragstart', dragStart);
+    dragItems.addEventListener('dragend', dragEnd);
+  }
+
+  for (const dropItem of droppable) {
+    dropItem.addEventListener('dragenter', dragEnter);
+    dropItem.addEventListener('dragover', dragOver);
+    dropItem.addEventListener('dragleave', dragLeave);
+    dropItem.addEventListener('drop', drop);
+  }
+});
+
+window.testA = function() {
+  document.querySelector('.testB').style.display = 'none';
+  document.querySelector('.testA').style.display = 'block';
+};
+
+window.testB = function() {
+  document.querySelector('.testB').style.display = 'block';
+  document.querySelector('.testA').style.display = 'none';
+};
